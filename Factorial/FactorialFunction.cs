4b8c52f2e8c0e3b32ca3d8f5a18e4f33d7acab55
f1b <33136217+f1b@users.noreply.github.com>
2022-12-27 20:53:13 +0300

    Init

---
+++ Factorial/FactorialFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Factorial
+{
+    public static class FactorialFunction
+    {
+        /// <summary>
+        /// Computes factorial
+        /// </summary>
+        /// <param name="n">Number</param>
+        /// <returns></returns>
+        public static double ComputeFactorial(int n)
+        {
+            EnsureNIsNotNegative(n);
+            return ComputeFactorialRecursively(n);
+        }
+
+        private static void EnsureNIsNotNegative(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("n is negative");
+            }
+        }
+
+        private static double ComputeFactorialRecursively(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return n * ComputeFactorialRecursively(n - 1);
+        }
+    }
+}
---
+++ FactorialUnitTest/FactorialFunctionUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Factorial.FactorialFunction;
+
+namespace FactorialUnitTest
+{
+    [TestClass]
+    public class FactorialFunctionUnitTest
+    {
+        private const double delta = 0.0001;
+
+        [TestMethod]
+        public void TestFact0()
+        {
+            int a = 0;
+            double factorial = ComputeFactorial(a);
+            Assert.AreEqual(1.0, factorial, delta);
+        }
+
+        [TestMethod]
+        public void TestFact1()
+        {
+            int a = 1;
+            double factorial = ComputeFactorial(a);
+            Assert.AreEqual(1.0, factorial, delta);
+        }
+
+        [TestMethod]
+        public void TestFact4()
+        {
+            int a = 4;
+            double factorial = ComputeFactorial(a);
+            Assert.AreEqual(24.0, factorial, delta);
+        }
+
+        [TestMethod]
+        public void TestFactExceptionCatch()
+        {
+            int a = -99999;
+            Assert.ThrowsException<ArgumentException>(() => ComputeFactorial(a));
+        }
+    }
+}
